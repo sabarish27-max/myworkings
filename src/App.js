f3fdaecc0a20a78fe2e9d1a6511b931e02801634
sabarish27-max <18bcasabarish@gmail.com>
2023-07-15 13:17:15 +0530

    all files are uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Importexport from './Importexport.js'
+import { variable,importfunction } from './Importexport.js';
+const App=()=>{
+    const template="also i am triing to print template litrals"
+    return(
+        <>
+<Importexport />
+<h1>{`hello iam just trying the import and export in react ${variable} ,${template}`}</h1>
+{/* <h2>{`Here is the expmle for importing and exporting function in the react ${importfunction()}`}</h2> */}
+<h1>{importfunction()}</h1>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Importexport.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const Importexport=()=>{
+    return(
+        <>
+<h1>i am a app component</h1>
+
+        </>
+    )
+}
+
+const importfunction=()=>{
+    return(
+        <h1>hello</h1>
+    )
+
+}
+const variable ="how to import and export a varaible in react"
+export default Importexport;
+export {variable,importfunction}
